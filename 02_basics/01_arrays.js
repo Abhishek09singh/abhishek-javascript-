1157a1c0ec10b9fa769fe32578ba7ec2077f8367
Abhishek09singh <162432428+Abhishek09singh@users.noreply.github.com>
2025-05-19 03:25:37 +0000

    arrays in basics part2

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+const myArr = [0, 1, 2, 3, 4]
+const myHeros = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[4]);
+
+// Arrays method 
+
+// myArr.push(6)
+// myArr.pop()
+
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(2));
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+// Slice and Splice
+
+log
